d5cacef51113d15de62d93fa63aae16cdabb6e59
Umut <69711134+umutsaydam@users.noreply.github.com>
2022-06-21 12:29:59 +0300

    Create sqlTask9.sql

---
+++ sqlTask9.sql
@@ -0,0 +1,16 @@
+-- city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+  
+  SELECT country, city FROM country
+  INNER JOIN city ON city.city_id = country.country_id;
+
+-- customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte
+-- görebileceğimiz INNER JOIN sorgusunu yazınız.
+  
+  SELECT payment_id, first_name, last_name FROM payment
+  INNER JOIN customer ON customer.customer_id = payment.customer_id;*/
+
+-- customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz
+-- INNER JOIN sorgusunu yazınız.
+  
+  SELECT rental_id, first_name, last_name FROM payment
+  INNER JOIN customer ON customer.customer_id = payment.customer_id;
